1dcee786ee38921a3e6287157338e6d4d1107a6d
Adil <send2adil.ahmed@gmail.com>
2026-04-18 13:17:04 +0530

    Phase 2: fix missing index include for tree_from_index

--- tree.c
+++ tree.c
@@ -10,6 +10,7 @@
 //   "100644 hello.txt\0" followed by 32 raw bytes of SHA-256
 
 #include "tree.h"
+#include "index.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -130,8 +131,33 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 //
 // Returns 0 on success, -1 on error.
 int tree_from_index(ObjectID *id_out) {
-    // TODO: Implement recursive tree building
-    // (See Lab Appendix for logical steps)
-    (void)id_out;
-    return -1;
+    Index index;
+
+    if (index_load(&index) < 0) return -1;
+
+    Tree tree;
+    tree.count = 0;
+
+    for (int i = 0; i < index.count; i++) {
+        IndexEntry *ie = &index.entries[i];
+
+        TreeEntry *te = &tree.entries[tree.count++];
+
+        te->mode = ie->mode;
+        te->hash = ie->hash;
+
+        const char *name = strrchr(ie->path, '/');
+        if (name) name++; else name = ie->path;
+
+        strncpy(te->name, name, sizeof(te->name));
+    }
+
+    void *data;
+    size_t len;
+    if (tree_serialize(&tree, &data, &len) < 0) return -1;
+
+    int rc = object_write(OBJ_TREE, data, len, id_out);
+
+    free(data);
+    return rc;
 }
